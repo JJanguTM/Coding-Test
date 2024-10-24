e3da6e0e8dbbf52e74c021bd342969ed9ad3ba58
Taemin <86860744+JJanguTM@users.noreply.github.com>
2024-10-24 19:09:01 +0900

    백준 2751 정답

---
+++ Project1/CT_1/CT_1_Num7.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int N;
+
+	cin >> N;
+
+	vector<int> arr(N);
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> arr[i];
+	}
+
+	sort(arr.begin(), arr.end());
+
+
+	for (int i = 0; i < N; i++)
+	{
+		cout << arr[i] << '\n';
+	}
+	
+}
+//개행처리 endl 과 '\n' 의 차이
